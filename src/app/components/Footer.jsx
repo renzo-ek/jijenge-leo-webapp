da5118ef3c00dc78c7b2d8df965161ce330e0f29
Imoite-zn <lawrenceimoite@gmail.com>
2024-06-26 07:05:25 -0500

    new

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>  
+          <Image
+              src="/images/logo.png"
+              alt="ISA"
+              width={60}
+              height={40}
+            />
+            </span>
+        <p className="text-slate-600">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
