0e1b7af013417a655806604de09a9997b10f62a2
renzo-ek <imoite.l@autochek.africa>
2025-09-15 18:43:25 +0300

    navlink ed

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
-      className="block mx-5 py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white font-mono"
+      className="block mx-5 py-2 pl-3 pr-4 text-gray-600 dark:text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-amber-600 dark:hover:text-white font-mono transition-colors duration-300"
     >
       {title}
     </Link>
